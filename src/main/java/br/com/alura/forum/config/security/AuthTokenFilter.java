b3b93d6db60114876661fb4799941227e0092b5d
Bruno Barbosa <bruno.barbosa.cs@gmail.com>
2021-03-01 23:06:43 -0300

    Getting token from request header 'Authorization'

---
+++ src/main/java/br/com/alura/forum/config/security/AuthTokenFilter.java
@@ -0,0 +1,31 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String token = getToken(request);
+        System.out.println(token);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        final String token = request.getHeader("Authorization");
+
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return token.substring(7, token.length());
+    }
+}
